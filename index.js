0b7d1e855232dae1a31076010de6c7cb0e6d877b
Cinthia Quadrado <92656147+cinthiaquadrado@users.noreply.github.com>
2022-02-26 16:22:49 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+function calculate() {
+    var height=(document.getElementById("height").value)/100
+    var weight=document.getElementById("weight").value
+
+    var imc=weight/(height**2);
+    var text=""
+    if(imc<18.50) {
+        text="Você está magro"
+    } else if(imc<24.9){
+        text="Você está com um peso regular"
+    } else if(imc<29.9){
+        text="Você está com sobrepeso"
+    } else if(imc<39.9){
+        text="Você está com obesidade"
+    } else if(imc>39.9){
+        text="Você está com obesidade mórbida"
+    }
+
+    document.getElementById("result").innerText=text
+}
